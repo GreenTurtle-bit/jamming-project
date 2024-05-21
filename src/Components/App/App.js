4b7ded816e63b9dcdbaa52875c15053f9b52ba22
Peter <peter.sangtopo@hotmail.com>
2024-05-21 22:15:42 +0100

    Implimenting statics

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,6 +6,42 @@ import { Playlist } from "../Playlist/Playlist";
 
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [
+        {
+          name: 'Example Track Name',
+          artist: 'Example Track Artist',
+          album: 'Example Track Album',
+          id: 1,
+        },
+        {
+          name: 'Example Track Name 2',
+          artist: 'Example Track Artist 2',
+          album: 'Example Track Album 2',
+          id: 2,
+        },
+        
+      ],
+      playlistName: 'Example PLaylist',
+      playlistTrack: [
+        {
+          name: 'Example PLaylist Track Name',
+          artist: 'Example PLaylist Track Artist',
+          album: 'Example PLaylist Track Album',
+          id: 3,
+        },
+        {
+          name: 'Example PLaylist Track Name 4',
+          artist: 'Example PLaylist Track Artist 4',
+          album: 'Example PLaylist Track Album 4',
+          id: 4,
+        }
+      ]
+    };
+  }
+
   render() {
     return (
       <div>
@@ -15,10 +51,12 @@ class App extends React.Component {
           <SearchBar />
           <div className='App-playlist'>
             {/*  */}
-            <SearchResults />
+            <SearchResults searchResults={this.state.searchResults}/>
 
             {/*  */}
-            <Playlist />
+            <Playlist 
+            playlistName={this.state.playlistName} 
+            playlistTrack={this.state.playlistTrack}/>
           </div>
         </div>
       </div>
--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import './Playlist.css'
+import './Playlist.css';
+import { TrackList } from "../TrackList/TrackList";
 
 export class Playlist extends React.Component {
     render() {
@@ -7,6 +8,7 @@ export class Playlist extends React.Component {
             <div className='Playlist'>
                 <input value='New Playlist'/>
                 {/*  */}
+                <TrackList tracks={this.props.playlistTrack}/>
                 <button className='Playlist-save'>SAVE TO SPOTIFY</button>
             </div>
         );
