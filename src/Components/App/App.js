7199007e784b419619969c7cea723cd4f5c1048e
Peter <peter.sangtopo@hotmail.com>
2024-05-24 16:58:54 +0100

    adding save playlist function

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -42,6 +42,9 @@ class App extends React.Component {
     };
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
+    
   }
 
   addTrack(track) {
@@ -62,7 +65,15 @@ class App extends React.Component {
     this.setState({ playlistTracks: isPresent });
   }
 
-  
+  updatePlaylistName(name) {
+    this.setState({ playlistName: name })
+  }
+
+  savePlaylist() {
+    const trackURIs = this.state.playlistTracks.map((track) => track.uri);
+}
+
+
 
   render() {
     return (
@@ -81,7 +92,9 @@ class App extends React.Component {
             <Playlist 
             playlistName={this.state.playlistName} 
             playlistTracks={this.state.playlistTracks}
-            onRemove={this.removeTrack}/>
+            onRemove={this.removeTrack}
+            onNameChange={this.updatePlaylistName}
+            onSave={this.savePlaylist}/>
           </div>
         </div>
       </div>
